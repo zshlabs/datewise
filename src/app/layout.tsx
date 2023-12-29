c7caf8385460eab4ba738984b89bb8bb54ac097b
Anurag30112003 <anuragsharma011011@gmail.com>
2023-12-29 17:35:11 +0530

    feat:- shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Analytics/>
+        <Analytics />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
