a1311985f514260457efd61801bda2de421aa36f
Anurag30112003 <anuragsharma011011@gmail.com>
2023-12-30 22:48:27 +0530

    feat:- settings page

---
+++ src/app/(application)/settings/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <div>Settings</div>
+    </main>
+  );
+}
