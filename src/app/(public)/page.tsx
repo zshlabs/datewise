7b790193eab2e0b8863dfe9ac38074b49fc16657
Anurag30112003 <anuragsharma011011@gmail.com>
2024-01-01 15:18:53 +0530

    feat:- variable landing page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,19 @@
 // import Image from "next/image";
 import Link from "next/link";
 import { auth, currentUser } from "@clerk/nextjs";
+import { env } from "@/lib/env";
 
 export default function Home() {
   const { userId } = auth();
+
+  if (env.NODE_ENV === "production") {
+    return (
+      <main className="flex flex-col items-center justify-between p-24">
+        <div>Datewise - A better calendar</div>
+        <div className="font-semibold">coming soon</div>
+      </main>
+    );
+  }
 
   return (
     <main className="flex flex-col items-center justify-between p-24">
