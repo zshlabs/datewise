551c5052d24290c163bdede52ea70f86eec40dc3
Anurag30112003 <anuragsharma011011@gmail.com>
2024-01-06 03:11:08 +0530

    ui: crackeddevs product hunt

---
+++ src/app/(public)/cd/page.tsx
@@ -0,0 +1,9 @@
+import { TextRevealCardPreview } from "../components/TextRevealCardPreview";
+
+export default function page() {
+  return (
+    <div>
+      <TextRevealCardPreview />
+    </div>
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   // also public route /api/post/getAll
-  publicRoutes: ["/", "/auth/login", "/auth/register"],
+  publicRoutes: ["/", "/auth/login", "/auth/register", "/cd"],
 });
 
 export const config = {
