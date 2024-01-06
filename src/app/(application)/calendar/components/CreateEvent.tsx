4c473369d21bd63e3276917bfa4d7d1d9a26c0bf
Anurag30112003 <anuragsharma011011@gmail.com>
2024-01-07 03:31:55 +0530

    refactor: create event

--- src/app/(application)/calendar/components/CreateEvent.tsx
+++ src/app/(application)/calendar/components/CreateEvent.tsx
@@ -1,15 +1,93 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { IconPlus } from "@tabler/icons-react";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { createEvent } from "@/app/(public)/actions";
+import { useAuth } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  date: z.date().min(new Date(), {
+    message: "Date must be in the future.",
+  }),
+});
 
 export default function CreateEvent() {
+  const { userId } = useAuth();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      date: new Date(Date.now() + 1000 * 60 * 60 * 24),
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
-    <div>
-      <Button variant={"outline"}>
-        {" "}
-        <IconPlus /> Add event
-      </Button>
-    </div>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">
+          {" "}
+          <IconPlus /> Create Event
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Create a event</AlertDialogTitle>
+        </AlertDialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name of the event</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
