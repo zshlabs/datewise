8d1c934db5b69e81c41e0eb9f0e104adc9322a29
pulimoodan <akbaraliongo@gmail.com>
2024-02-05 12:42:44 +0530

    ui: adds day active cursor

---
+++ src/app/(application)/calendar/components/DayActiveCursor.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DayActiveCursor() {
+  return (
+    <div className="h-[23px] w-[100%] top-[50px] left-0 border-x-2 border-blue-600 absolute flex items-center">
+      <div className="h-[3px] w-[100%] bg-blue-600"></div>
+    </div>
+  );
+}
+
+export default DayActiveCursor;
