63687bbcdf3f9dc4a15c7e669fde27e80c82f1db
pulimoodan <akbaraliongo@gmail.com>
2024-02-10 19:32:39 +0530

    ui: updates time active cursor

--- src/app/(application)/calendar/components/TimeActiveCursor.tsx
+++ src/app/(application)/calendar/components/TimeActiveCursor.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-function TimeActiveCursor() {
+interface Props {
+  now: Date;
+}
+
+function TimeActiveCursor({ now }: Props) {
   return (
-    <div className="absolute h-[10px] left-[80%] top-[50%] translate-y-[-50%] border-l-2 border-blue-300 flex items-center">
-      <div className="h-[2px] w-[100vw] bg-blue-300"></div>
+    <div
+      className={`absolute flex items-center w-[100%] justify-center text-blue-500`}
+      style={{
+        top: `${Math.round((now.getMinutes() / 60) * 100)}%`,
+      }}
+    >
+      {now.toLocaleTimeString("en", {
+        hour: "numeric",
+        minute: "numeric",
+      })}
+      <div className="absolute h-[10px] left-[80%] top-[50%] translate-y-[-50%] border-l-2 border-blue-300 flex items-center">
+        <div className="h-[2px] w-[100vw] bg-blue-300"></div>
+      </div>
     </div>
   );
 }
